a8ed95ceb37419e6ebec8f88c0548d2d5fc026e2
Bruna <porto.acad@gmail.com>
2023-08-09 18:18:21 -0300

    first commit

---
+++ cod.java
@@ -0,0 +1,15 @@
+public class cod {
+    public static void main(String[] args) throws Exception {
+        System.out.println("\nHi!");
+        String version = System.getProperty("java.version");
+        System.out.println("Running Java Version "+version+"\n");
+        String nomeSistema = System.getProperty("os.name");
+        System.out.println("outras coisas");
+        System.out.println("Sistema Operacional: " + nomeSistema);
+        System.out.println("funciona satanas");
+        System.out.println("Novos testes");  
+        System.out.println("chatice");
+        System.out.println("insuportabilidade");
+        
+    }
+}
